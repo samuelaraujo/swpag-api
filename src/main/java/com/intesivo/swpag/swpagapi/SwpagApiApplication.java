a55912ae649a8aecfca79abc2f1a37b6bbf596eb
samuelaraujo <samuelbrazdearaujo@hotmail.com>
2024-03-23 15:46:09 -0500

    versão inicial

---
+++ src/main/java/com/intesivo/swpag/swpagapi/SwpagApiApplication.java
@@ -0,0 +1,13 @@
+package com.intesivo.swpag.swpagapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwpagApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwpagApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/intesivo/swpag/swpagapi/SwpagApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.intesivo.swpag.swpagapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwpagApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
