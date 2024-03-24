a0caef7887e122bd5f48409be2c9d1da2b946546
samuelaraujo <samuelbrazdearaujo@hotmail.com>
2024-03-23 20:59:20 -0500

    Buscando dados pelo cliente repository

--- src/main/java/com/intesivo/swpag/swpagapi/controllers/ClienteController.java
+++ src/main/java/com/intesivo/swpag/swpagapi/controllers/ClienteController.java
@@ -1,6 +1,11 @@
 package com.intesivo.swpag.swpagapi.controllers;
 
 import com.intesivo.swpag.swpagapi.domain.model.Cliente;
+import com.intesivo.swpag.swpagapi.domain.repositoy.ClienteRepositoy;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.apache.catalina.Manager;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.Mapping;
@@ -11,14 +16,14 @@ import java.util.List;
 @RestController
 public class ClienteController {
 
-    // Simulação de uma lista de clientes
-    private List<Cliente> clientes;
+    @Autowired
+    private ClienteRepositoy clienteRepositoy;
+
 
     // Endpoint para retornar a lista de clientes
     @GetMapping("/clientes")
     public List<Cliente> getClientes() {
-        return clientes;
-
+        return clienteRepositoy.findAll();
     }
 
     /* Endpoint para retornar a lista de clientes inicial
