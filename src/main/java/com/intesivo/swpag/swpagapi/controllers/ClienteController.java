344427a06df8413d03bc7c4c720c1ec8255c9575
samuelaraujo <samuelbrazdearaujo@hotmail.com>
2024-03-23 19:20:58 -0500

    Lista vindo direto do bando de dados com as entidades clinte e Endereco

--- src/main/java/com/intesivo/swpag/swpagapi/controllers/ClienteController.java
+++ src/main/java/com/intesivo/swpag/swpagapi/controllers/ClienteController.java
@@ -1,26 +1,42 @@
 package com.intesivo.swpag.swpagapi.controllers;
 
 import com.intesivo.swpag.swpagapi.domain.model.Cliente;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.Mapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
 import java.util.List;
 
 @RestController
 public class ClienteController {
+    @PersistenceContext
+    private EntityManager manager;
 
-    // Simulação de uma lista de clientes
-    private List<Cliente> clientes;
 
     // Endpoint para retornar a lista de clientes
     @GetMapping("/clientes")
     public List<Cliente> getClientes() {
-        return clientes;
+       return manager.createQuery("from Cliente", Cliente.class)
+               .getResultList();
 
     }
+    // Endpoint para retornar a lista de clientes
 
+    /*@GetMapping("/clientes")
+    public List<Cliente> getClientes() {
+        var cliente = new Cliente(1L,
+                "samuel araujo",
+                "66293820215",
+                "samueolbraz@araujo",
+                "6899954248");
+        return Arrays.asList(cliente);
+
+    }
+*/
     /* Endpoint para retornar a lista de clientes
     @GetMapping("/clientes1")
     public String getClientes1() {
